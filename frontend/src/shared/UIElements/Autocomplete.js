275efbf8285e1e72a487575f743d536157e87d52
hasanbasbous <hasan.basbous@lau.edu>
2022-11-17 17:16:43 +0200

    Added autocomplete address bar to the pothole map

---
+++ frontend/src/shared/UIElements/Autocomplete.js
@@ -0,0 +1,93 @@
+import React, { useRef, useEffect, useState } from 'react';
+import { GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
+import './Map.css';
+import Button from '../FormElements/Button';
+
+const AutoComplete = (props) => {
+	const autoCompleteRef = useRef();
+	const inputRef = useRef();
+	// const options = {
+	// 	componentRestrictions: { country: 'ng' },
+	// 	fields: ['address_components', 'geometry', 'icon', 'name'],
+	// 	types: ['establishment'],
+	// };
+	const [zoom, setZoom] = useState(null);
+	useEffect(() => {
+		autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+			inputRef.current
+			// options
+		);
+		autoCompleteRef.current.addListener('place_changed', async function () {
+			const place = await autoCompleteRef.current.getPlace();
+			console.log(place.geometry.location.lat());
+			setZoom(
+				new window.google.maps.LatLng(
+					place.geometry.location.lat(),
+					place.geometry.location.lng()
+				)
+			);
+		});
+	}, [zoom]);
+
+	const potholes = props.potholes;
+	const [activeMarker, setActiveMarker] = useState(null);
+
+	const handleActiveMarker = (marker) => {
+		if (marker === activeMarker) {
+			return;
+		}
+		setActiveMarker(marker);
+	};
+
+	const handleOnLoad = (map) => {
+		const google = window.google;
+		const bounds = new google.maps.LatLngBounds();
+		potholes.forEach(({ location }) => bounds.extend(location));
+		map.fitBounds(bounds);
+	};
+
+	return (
+		<React.Fragment>
+			<div id="search-bar">
+				<label>Search address: </label>
+				<input ref={inputRef} />
+			</div>
+
+			<GoogleMap
+				onLoad={handleOnLoad}
+				onClick={() => setActiveMarker(null)}
+				mapContainerClassName="mapstyle"
+				center={zoom}
+			>
+				{potholes.map(({ id, image, location, date }) => (
+					<Marker
+						key={id}
+						position={location}
+						onClick={() => handleActiveMarker(id)}
+					>
+						{activeMarker === id ? (
+							<InfoWindow onCloseClick={() => setActiveMarker(null)}>
+								<div>
+									<div>
+										<img
+											src={`/images/${image}`}
+											alt="frem.jpg"
+											width="200"
+											height="200"
+										/>
+									</div>
+									{/* <div>Driver: {driver_name}</div>
+                                <div>Intensity: {intensity}g</div> */}
+									<div>Date: {date}</div>
+								</div>
+							</InfoWindow>
+						) : null}
+					</Marker>
+				))}
+			</GoogleMap>
+			<Button to="/features">Go Back</Button>
+		</React.Fragment>
+	);
+};
+
+export default AutoComplete;
--- frontend/src/shared/UIElements/MapPotholes.js
+++ frontend/src/shared/UIElements/MapPotholes.js
@@ -1,61 +1,64 @@
-import React, { useState } from 'react';
-import { GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
+import React from 'react';
+// import { GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
 import './Map.css';
-import Button from '../FormElements/Button';
+// import Button from '../FormElements/Button';
+import AutoComplete from './Autocomplete';
 
 const MapPotholes = (props) => {
-	const potholes = props.potholes;
-	const [activeMarker, setActiveMarker] = useState(null);
+	// const potholes = props.potholes;
+	// const [activeMarker, setActiveMarker] = useState(null);
 
-	const handleActiveMarker = (marker) => {
-		if (marker === activeMarker) {
-			return;
-		}
-		setActiveMarker(marker);
-	};
+	// const handleActiveMarker = (marker) => {
+	// 	if (marker === activeMarker) {
+	// 		return;
+	// 	}
+	// 	setActiveMarker(marker);
+	// };
 
-	const handleOnLoad = (map) => {
-		const google = window.google;
-		const bounds = new google.maps.LatLngBounds();
-		potholes.forEach(({ location }) => bounds.extend(location));
-		map.fitBounds(bounds);
-	};
+	// const handleOnLoad = (map) => {
+	// 	const google = window.google;
+	// 	const bounds = new google.maps.LatLngBounds();
+	// 	potholes.forEach(({ location }) => bounds.extend(location));
+	// 	map.fitBounds(bounds);
+	// };
 
 	return (
-		<React.Fragment>
-			<GoogleMap
-				onLoad={handleOnLoad}
-				onClick={() => setActiveMarker(null)}
-				mapContainerClassName="mapstyle"
-			>
-				{potholes.map(({ id, image, location, date }) => (
-					<Marker
-						key={id}
-						position={location}
-						onClick={() => handleActiveMarker(id)}
-					>
-						{activeMarker === id ? (
-							<InfoWindow onCloseClick={() => setActiveMarker(null)}>
-								<div>
-									<div>
-										<img
-											src={`/images/${image}`}
-											alt="frem.jpg"
-											width="200"
-											height="200"
-										/>
-									</div>
-									{/* <div>Driver: {driver_name}</div>
-									<div>Intensity: {intensity}g</div> */}
-									<div>Date: {date}</div>
-								</div>
-							</InfoWindow>
-						) : null}
-					</Marker>
-				))}
-			</GoogleMap>
-			<Button to="/features">Go Back</Button>
-		</React.Fragment>
+		// <React.Fragment>
+		// 	<AutoComplete />
+		// 	<GoogleMap
+		// 		onLoad={handleOnLoad}
+		// 		onClick={() => setActiveMarker(null)}
+		// 		mapContainerClassName="mapstyle"
+		// 	>
+		// 		{potholes.map(({ id, image, location, date }) => (
+		// 			<Marker
+		// 				key={id}
+		// 				position={location}
+		// 				onClick={() => handleActiveMarker(id)}
+		// 			>
+		// 				{activeMarker === id ? (
+		// 					<InfoWindow onCloseClick={() => setActiveMarker(null)}>
+		// 						<div>
+		// 							<div>
+		// 								<img
+		// 									src={`/images/${image}`}
+		// 									alt="frem.jpg"
+		// 									width="200"
+		// 									height="200"
+		// 								/>
+		// 							</div>
+		// 							{/* <div>Driver: {driver_name}</div>
+		// 							<div>Intensity: {intensity}g</div> */}
+		// 							<div>Date: {date}</div>
+		// 						</div>
+		// 					</InfoWindow>
+		// 				) : null}
+		// 			</Marker>
+		// 		))}
+		// 	</GoogleMap>
+		// 	<Button to="/features">Go Back</Button>
+		// </React.Fragment>
+		<AutoComplete potholes={props.potholes} />
 	);
 };
 
